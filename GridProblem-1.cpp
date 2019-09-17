03417cd5aa9cce286e8b1f6ab21792d9a965fceb
DHRUV-EDDI <dhruv2018570@gmail.com>
2019-09-17 14:57:40 +0000

    commit

--- GridProblem-1.cpp
+++ GridProblem-1.cpp
@@ -2,25 +2,20 @@
 using namespace std;
 int n,m,c=INT_MAX;
 vector< vector< pair<int,int> > > v;
-int dp[1000][1000];
-int Min_power(int ri,int ci,int p)
+void Min_power(int ri,int ci,int p)
 {
     if(ri < 0 || ci < 0 || ri >= n || ci >= m)
-    return INT_MAX;
+    return ;
     if(ri == n-1 and ci == m-1)
     {
         c = min(c,p);
-        return c;
+        return;
     }
-    if( dp[ri][ci] != -1)
-        return dp[ri][ci];
-    int x = INT_MAX;
+
     for(int z=1;z<=v[ri][ci].first;z++)
-        x = min(x,Min_power(ri,ci+z,p+1));
+        Min_power(ri,ci+z,p+1);
     for(int z=1;z<=v[ri][ci].second;z++)
-        x= min(x,Min_power(ri+z,ci,p+1));
-    dp[ri][ci] = x;
-    return x;
+        Min_power(ri+z,ci,p+1);
 }
 int main()
 {
@@ -28,13 +23,13 @@ int main()
     cin.tie(0);
     cin>>n>>m;
     v.resize(n,vector< pair<int,int> >(m));
-    memset(dp,-1,sizeof(dp));
     for(int i=0;i<n;i++)
         for(int j=0;j<m;j++)
             cin>>v[i][j].first;
     for(int i=0;i<n;i++)
         for(int j=0;j<m;j++)
             cin>>v[i][j].second;
-    cout<<Min_power(0,0,0)<<endl;
+    Min_power(0,0,0);
+    cout<<c<<endl;
     return 0;
 }
